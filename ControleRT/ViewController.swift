28bb7c89c7d85e3d1856021c09d5a62c4be12034
Aloisio <aloisio@aloisios-mac-pro.local>
2018-05-10 14:14:28 -0300

    Initial Commit

---
+++ ControleRT/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ControleRT
+//
+//  Created by Aloisio on 10/05/18.
+//  Copyright © 2018 Aloisio. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
