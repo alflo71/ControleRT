8e4af35125c81f05407f0648b0590d6e7642cf6a
Aloisio <aloisio@aloisios-mac-pro.local>
2018-05-11 09:29:22 -0300

    Criando conexão dos objectos da view com o BDRealm

--- ControleRT/GrupoViews/viewCadastroCliente.swift
+++ ControleRT/GrupoViews/viewCadastroCliente.swift
@@ -13,12 +13,30 @@ class viewCadastroCliente: NSViewController {
     
     let realm = try! Realm()
     
+    @IBOutlet weak var textClienteID: NSTextField!
+    @IBOutlet weak var textNome: NSTextField!
+    @IBOutlet weak var textTelefone: NSTextField!
+    @IBOutlet weak var textEndereco: NSTextField!
+    @IBOutlet weak var textEmail: NSTextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
     }
     
     @IBAction func btnUpdate(_ sender: Any) {
+        
+        try! realm.write {
+            var newCliente = Cliente()
+            newCliente.clienteID = textClienteID.stringValue
+            newCliente.nome = textNome.stringValue
+            newCliente.telefone = textTelefone.stringValue
+            newCliente.endereco = textEndereco.stringValue
+            newCliente.email = textEmail.stringValue
+            
+            realm.add(newCliente.self)
+        }
         
     }
     
